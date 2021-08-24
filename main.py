f2b1c1f14304e131c1b04af6e8c8f2f754d9980f
Min Kyu Kim <devsmile@twc.co.kr>
2021-08-24 11:58:56 +0900

    first commit on develop branch

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def health_check():
     return "Hello world"
 
 
+@app.get("/article/")
+def get_articles():
+    return {"data": [1,2,3,4,5,6,7,8]}
+
 @app.get("/article/{id}")
-def this_is_world(id=3):
+def get_article(id=3):
     return {"data": id}
+
+@app.get("/article/{id}/comments")
+def get_comments(id):
+    return {"data": [1,2,3,4,5]}
+
